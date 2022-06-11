79020811a5b0e5115dceb877414957922d77a1ba
rong5026 <rong5026@naver.com>
2022-06-11 21:56:18 +0900

    [ADD] 유저 정보 수정 - body : 수정할 정보 , params : 수정할 회원 ID

--- routes/users.js
+++ routes/users.js
@@ -2,38 +2,63 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models');
 
-
-router.route('/')
-    .get(async(req,res,next)=>{      //유저조회
-        try{
-            const users =await models.User.findAll({
-                attributes : ['user_id','name']
-            });
-            res.json(users);
-        } catch (err) {
-            console.error(err);
-            next(err);
-        }
+// 유저 전체 조회
+router.get('/', (req, res, next) => {
+    models.User.findAll()
+      .then((users) => {
+        res.json(users);
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      })
+  });
+  
+  // 회원가입
+  router.post('/register', (req, res, next) => {
+    models.User.create({
+        user_id : req.body.user_id,
+        password : req.body.password,
+        name : req.body.name,
+        phone : req.body.phone,
+        address : req.body.address,
+        gender : req.body.gender,
+        email : req.body.email
     })
-    .post(async(req,res,next)=>{      // 유저 등록
-        try{
-            const user = await models.User.create({
-                user_id : req.body.user_id,
-                password : req.body.password,
-                name : req.body.name,
-                phone : req.body.phone,
-                address : req.body.address,
-                gender : req.body.gender,
-                email : req.body.email
-            });
-            console.log(user);
-            res.status(201).json(user);
-        } catch (err) {
-            console.error(err);
-            next(err);
-        }
-    });
+      .then((result) => {
+        console.log(result);
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      })
+  })
 
+   // 유저 수정
+   router.put('/update/:user_id', (req, res, next) => {
+    models.User.update({
+        user_id : req.body.user_id,
+        password : req.body.password,
+        name : req.body.name,
+        phone : req.body.phone,
+        address : req.body.address,
+        gender : req.body.gender,
+        email : req.body.email
+    }, {
+        where : { user_id : req.params.user_id },
+    })
+      .then((result) => {
+        console.log(result);
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        console.error(err);
+        next(err);
+      })
+  })
+
+  
 
 
 module.exports = router;
